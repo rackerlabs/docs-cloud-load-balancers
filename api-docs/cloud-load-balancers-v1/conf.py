9dbeb860e455c12fd96c61254d62853947049e1b
Margaret Eker <margaret.eker@rackspace.com>
2016-06-20 22:07:44 -0500

    Configuration updates
    - Update exclude patterns to add getting-started examples folder
    - Add Sphinx RTD theme for local builds

--- api-docs/cloud-load-balancers-v1/conf.py
+++ api-docs/cloud-load-balancers-v1/conf.py
@@ -86,7 +86,7 @@ language = None
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 exclude_patterns = ['_build','samples','api-operations/methods/*','common-gs',
-                    'examples/*','release-notes/*']
+                    'getting-started/examples/*','release-notes/*']
 
 # The reST default role (used for this markup: `text`) to use for all
 # documents.
@@ -155,7 +155,7 @@ todo_include_todos = False
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-#html_theme = 'alabaster'
+html_theme = 'sphinx_rtd_theme'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
