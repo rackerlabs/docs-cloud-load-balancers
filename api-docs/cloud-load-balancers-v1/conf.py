6dd4807a99d8e2e1e879b18221b11dcff2b0eb74
Ravi <raviteja.valluri@rackspace.com>
2021-04-14 11:40:15 -0700

    HTML for Search, Header and Footer

--- api-docs/cloud-load-balancers-v1/conf.py
+++ api-docs/cloud-load-balancers-v1/conf.py
@@ -183,10 +183,10 @@ html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 
 # The name for this set of Sphinx documents.  If None, it defaults to
 # "<project> v<release> documentation".
-html_title = 'Rackspace Cloud Load Balancer API Documentation'
+# html_title = 'Rackspace Cloud Load Balancer API Documentation'
 
 # A shorter title for the navigation bar.  Default is the same as html_title.
-html_short_title = 'Rackspace Cloud Load Balancers'
+# html_short_title = 'Rackspace Cloud Load Balancers'
 
 # The name of an image file (relative to this directory) to place at the top
 # of the sidebar.
